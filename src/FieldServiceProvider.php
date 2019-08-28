9514f481d509aa2293dbc922bc3c8f797ba0f1f2
Sebastiano Bordieri <bordieris@artimedia.net>
2019-08-28 19:45:20 +0200

    Class names and namespaces refactoring

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -18,14 +18,14 @@ class FieldServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/extended-address.php' => base_path('config/extended-address.php'),
+                __DIR__.'/../config/extended-place.php' => base_path('config/extended-place.php'),
             ], 'config');
         }
 
         Nova::serving(function (ServingNova $event) {
-            Nova::script('extended_address_gmaps', $this->googleMapsSource());
-            Nova::script('extended_address', __DIR__.'/../dist/js/field.js');
-            Nova::style('extended_address', __DIR__.'/../dist/css/field.css');
+            Nova::script('extended_place_gmaps', $this->googleMapsSource());
+            Nova::script('extended_place', __DIR__.'/../dist/js/field.js');
+            Nova::style('extended_place', __DIR__.'/../dist/css/field.css');
         });
     }
 
@@ -36,7 +36,7 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/extended-address.php', 'extended-address');
+        $this->mergeConfigFrom(__DIR__.'/../config/extended-place.php', 'extended-place');
     }
 
     private function googleMapsSource()

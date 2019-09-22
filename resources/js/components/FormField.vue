8b0bfd14b125c8319d4f0f697ee409cf2f7eefee
Sebastiano Bordieri <bordieris@artimedia.net>
2019-09-22 18:55:01 +0200

    Working version for demo

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -11,7 +11,8 @@
             <p v-if="hasError" class="my-2 text-danger">
                 {{ firstError }}
             </p>
-
+            <input v-if="false" type="text" class="w-full form-control form-input form-input-bordered"
+                   :value="JSON.stringify(address)" />
         </template>
     </default-field>
 </template>
@@ -33,24 +34,42 @@
         props: ['resourceName', 'resourceId', 'field'],
         data: function () {
             return {
-                mapName: this.name + "-map",
+                mapName: this.name + "-map"
             }
         },
         mounted: function () {
             var map;
             const element = document.getElementById(this.mapName);
-            var lat = 40.730610;
-            var lng = -98.935242;
+            var latitudeFieldName = this.field.latitude;
+            var longitudeFieldName = this.field.longitude;
+            var cityFieldName = this.field.city;
+            var provinceFieldName = this.field.province;
+            this.address = {};
+            if (this.value.length>0 && this.isValidJSON(this.value)){
+                this.address = JSON.parse(this.value);
+            } else {
+                this.address.address = this.value;
+                let tmpLat = this.getElementValue(this.$parent, latitudeFieldName)
+                let tmpLng = this.getElementValue(this.$parent, longitudeFieldName)
+                if(tmpLat && tmpLng) {
+                    this.address.latlng = {"lat": tmpLat, "lng": tmpLng}
+                }
+                this.address.city = this.getElementValue(this.$parent, cityFieldName)
+                this.address.province = this.getElementValue(this.$parent, provinceFieldName)
+                this.address.formatted_address = this.address.address + ' ' +this.address.city
+            }
+            console.log(this.address);
+            var lat = 45.0430593;
+            var lng = 9.6725524;
+
 
             if (this.field.lat && this.field.lng){
                 lat = this.field.lat;
                 lng = this.field.lng;
             }
-            if (this.value){
-               var latlng =  this.value.split("|");
-               latlng = latlng[1].split(",");
-                lat = latlng[0];
-                lng = latlng[1];
+            if (this.address.latlng && this.address.latlng.lat && this.address.latlng.lng){
+                lat = this.address.latlng.lat;
+                lng = this.address.latlng.lng;
                 if (previousMarker) {
                     previousMarker.setMap(null);
                 }
@@ -61,7 +80,9 @@
             };
             map = new google.maps.Map(element, options);
             var previousMarker;
-            var address = this;
+
+            var extendedPlaceField = this; //address.formatted_address ? address.formatted_address : '' ;
+            extendedPlaceField.value = this.address.formatted_address ? this.address.formatted_address : '';
             previousMarker = new google.maps.Marker({
                 position: new google.maps.LatLng(lat, lng),
                 map: map
@@ -78,7 +99,49 @@
                 geocoder.geocode({'location': event.latLng}, function(results, status) {
                     if (status === 'OK') {
                         if (results[0]) {
-                            address.value = results[0].formatted_address+'|'+event.latLng.lat().toFixed(6)+','+event.latLng.lng().toFixed(6);
+                            //address = results[0].formatted_address+'|'+event.latLng.lat().toFixed(6)+','+event.latLng.lng().toFixed(6);
+                            this.address = {
+                                "formatted_address" : results[0].formatted_address,
+                                "latlng": {
+                                    "lat": event.latLng.lat().toFixed(6),
+                                    "lng": event.latLng.lng().toFixed(6)
+                                },
+                                "address": null
+                            };
+                            let pieces = results[0].address_components
+                            let cityname = {'city':null, 'locality':null}
+                            for(let i=0; i<pieces.length;i++){
+                                let value = pieces[i]
+                                if(value.types.includes('route')){
+                                    if(this.address.address===null){
+                                        this.address.address = value.short_name
+                                    } else {
+                                        this.address.address = value.short_name + ', ' + this.address.address
+                                    }
+                                    if(this.address.address=='Unnamed Road') this.address.address = ''
+                                } else if (value.types.includes('street_number')){
+                                    if(this.address.address===null){
+                                        this.address.address = value.short_name
+                                    } else {
+                                        this.address.address =  this.address.address + ', ' + value.short_name
+                                    }
+                                } else if (value.types.includes('administrative_area_level_2')){
+                                    this.address.province = value.short_name
+                                }  else if (value.types.includes('administrative_area_level_3')){
+                                    cityname.city = value.short_name
+                                }  else if (value.types.includes('locality') ){
+                                    cityname.locality = value.short_name
+                                }
+                                this.address.city = cityname.locality
+                                if(null==address.city) this.address.city = cityname.city
+                            }
+
+                            extendedPlaceField.value = this.address.formatted_address ? this.address.formatted_address : ''
+                            Nova.$emit(latitudeFieldName + '-value', this.address.latlng.lat)
+                            Nova.$emit(longitudeFieldName + '-value', this.address.latlng.lng)
+                            Nova.$emit(cityFieldName + '-value', this.address.city)
+                            Nova.$emit(provinceFieldName + '-value', this.address.province)
+                            extendedPlaceField.address = this.address;
                         } else {
                             window.alert('No results found');
                         }
@@ -100,15 +163,45 @@
             /**
              * Fill the given FormData object with the field's internal value.
              */
-            fill(formData) {
+            /*fill(formData) {
                 formData.append(this.field.attribute, this.value || '')
+            },*/
+            /**
+             * Fill the given FormData object with the field's internal value.
+             */
+            fill(formData) {
+                //formData.append(this.field.attribute, this.value || '')
+                formData.append(this.field.attribute, JSON.stringify(this.address) || '')
             },
-
             /**
              * Update the field's internal value.
              */
             handleChange(value) {
                 this.value = value
+            },
+            isValidJSON(stringifiedJSON){
+                return (/^[\],:{}\s]*$/.test(stringifiedJSON.replace(/\\["\\\/bfnrtu]/g, '@').
+                replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+                replace(/(?:^|:|,)(?:\s*\[)+/g, '')))
+            },
+            getElementValue(root, elemName){
+                let value = null
+                root.$children.forEach(component => {
+                    if (component.field !== undefined && component.field.attribute == elemName) {
+                        value = component.field.value
+                        console.log(component)
+                    }
+                })
+                return value
+            },
+            getElement(root, elemName){
+                let theComponent = null
+                root.$children.forEach(component => {
+                    if (component.field !== undefined && component.field.attribute == elemName) {
+                        theComponent = component;
+                    }
+                })
+                return theComponent
             }
         },
     }
--- src/ExtendedPlace.php
+++ src/ExtendedPlace.php
@@ -2,8 +2,10 @@
 
 namespace Bordieris\ExtendedPlace;
 
+use App\City;
 use Illuminate\Support\Facades\Config;
 use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class ExtendedPlace extends Field
 {
@@ -14,6 +16,10 @@ class ExtendedPlace extends Field
      */
     public $component = 'extended_place';
 
+    private $cityField = '';
+    private $cityIdField = '';
+    private $latField = '';
+    private $longField = '';
 
     /**
      * Create a new field.
@@ -43,5 +49,88 @@ class ExtendedPlace extends Field
         return $this->withMeta([
             'zoom' => $zoom
         ]);
+    }
+
+    public function city($city='city'){
+        if(!is_array($city)){
+            $city = [$city, $city.'_id'];
+        }
+        if(count($city)>1){
+            $this->cityField = $city[0];
+            $this->cityIdField = $city[1];
+        }
+
+        //return $this->withMeta([]);
+        return $this->withMeta([__FUNCTION__ => $this->cityField]);
+    }
+
+    /**
+     * Specify the field that contains the province.
+     *
+     * @param  string  $field
+     * @return $this
+     */
+    public function province($field)
+    {
+        return $this->withMeta([__FUNCTION__ => $field]);
+    }
+
+    /**
+     * Specify the field that contains the latitude.
+     *
+     * @param  string  $field
+     * @return $this
+     */
+    public function latitude($field)
+    {
+        $this->latField=$field;
+        return $this->withMeta([__FUNCTION__ => $field]);
+    }
+
+    /**
+     * Specify the field that contains the longitude.
+     *
+     * @param  string  $field
+     * @return $this
+     */
+    public function longitude($field)
+    {
+        $this->longField=$field;
+        return $this->withMeta([__FUNCTION__ => $field]);
+    }
+
+    /**
+     * Hydrate the given attribute on the model based on the incoming request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  string  $requestAttribute
+     * @param  object  $model
+     * @param  string  $attribute
+     * @return void
+     */
+    protected function fillAttributeFromRequest(NovaRequest $request,
+                                                $requestAttribute,
+                                                $model,
+                                                $attribute)
+    {
+        if ($request->exists($requestAttribute)) {
+            if($request[$requestAttribute] && $address = json_decode($request[$requestAttribute], true)) {
+                $model->{$attribute} = $address['address'];//address
+                $city = City::getByName($address['city']);
+                $model->{$this->latField} = $address['latlng']['lat'];
+                $model->{$this->longField} = $address['latlng']['lng'];
+                if (!$city) {
+                    $city = City::create([
+                        'name' => $address['city'],
+                        'province' => $address['province'],
+                        'external' => ($address['province'] != 'PC')
+                    ]);
+                }
+                $model->{$this->cityIdField} = $city->id;
+
+
+            }
+        }
+
     }
 }
